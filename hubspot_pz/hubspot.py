da8d29a5babbf764989b680a6caae9d15c8f93d9
nicolas.amadori <nicolas.amadori@studio.unibo.it>
2025-12-16 12:59:01 +0100

    fix: getAllDeals associations retrieval

--- hubspot_pz/hubspot.py
+++ hubspot_pz/hubspot.py
@@ -105,7 +105,7 @@ class HubspotPZ:
         return self._getAllObjects("companies", properties_names, associations_objects=["contacts"])
     
     def getAllDeals(self, properties_names: List[str]) -> List[Dict[str, str]]:
-        return self._getAllObjects("0-3", properties_names, associations_objects=["contacts, companies"])
+        return self._getAllObjects("0-3", properties_names, associations_objects=["contacts", "companies"])
 
     ##########Single Objects############################################
     
